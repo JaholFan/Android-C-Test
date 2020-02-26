3c21cf10dc99f54255cd7ef74e6f790151e92b84
Jahol Fan <fjh@lonbon.com>
2020-02-26 16:31:08 +0800

    Android C test

---
+++ test.c
@@ -0,0 +1,26 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <errno.h>
+#include <unistd.h>
+#include <time.h>
+#include <dirent.h>
+
+#include <sys/types.h>
+#include <sys/file.h>
+#include <sys/ioctl.h>
+#include <sys/select.h>
+#include <sys/socket.h>
+#include <sys/ioctl.h>
+#include <sys/mman.h>
+#include <netinet/in.h>
+#include <netinet/if_ether.h>
+#include <net/if.h>
+#include <net/if_arp.h>
+#include <arpa/inet.h>
+#include <sys/stat.h>
+
+int main(void)
+{
+    printf("hello world\n");
+}
